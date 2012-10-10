cf7e695f707f835800734fcbe4f440ef99e2369d
Axel Burri <axel@tty0.ch>
2012-10-10 14:11:30 +0200

    added gallery.org example (finally), along with all gallery templates

---
+++ examples/gallery.org/scripts/showhide.js
@@ -0,0 +1,13 @@
+function show(id) {
+    document.getElementById(id).style.display = 'block';
+}
+function hide(id) {
+    document.getElementById(id).style.display = 'none';
+}
+function toggle(id) {
+    var ele=document.getElementById(id);
+    if(ele.style.display == 'block')
+	ele.style.display = 'none';
+    else
+	ele.style.display = 'block';
+}
