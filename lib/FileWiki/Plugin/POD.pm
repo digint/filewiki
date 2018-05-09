3e15049baac2e3c2a6c676a3bcc733f565131d71
Axel Burri <axel@tty0.ch>
2018-05-09 18:26:10 +0200

    globally replaced copyright year 2017 -> 2018 (license header)

--- lib/FileWiki/Plugin/POD.pm
+++ lib/FileWiki/Plugin/POD.pm
@@ -25,7 +25,7 @@ Axel Burri <axel@tty0.ch>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (c) 2011-2017 Axel Burri. All rights reserved.
+Copyright (c) 2011-2018 Axel Burri. All rights reserved.
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
